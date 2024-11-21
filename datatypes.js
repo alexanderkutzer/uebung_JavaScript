404d73a088fc0c877645b6a624e1b935355f702b
alexanderkutzer <kutzeralexander@web.de>
2024-11-21 18:53:33 +0100

    uebung

---
+++ datatypes.js
@@ -0,0 +1,14 @@
+let nameUser ="Alexander"
+let age = 26
+let likes = true 
+
+console.log(
+    "My name is " + 
+    nameUser +
+    " , I am " + 
+    age +
+    " years old and it is " +
+    likes +
+    " that I like programming."
+
+)
---
+++ loops.js
@@ -0,0 +1,5 @@
+var i = 1
+
+for (i; i < 11; i++) {
+    console.log(i);
+}
